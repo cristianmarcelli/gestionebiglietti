7d4f0dc05882fa162e742cc496eb137301a9e57c
Cristian <cristianma98@gmail.com>
2022-05-31 17:22:59 +0200

    implementazione filtro e logout

---
+++ src/main/java/it/prova/gestionebiglietti/model/Utente.java
@@ -0,0 +1,49 @@
+package it.prova.gestionebiglietti.model;
+
+public class Utente {
+
+	private String username;
+	private String password;
+	private String nome;
+	private String cognome;
+
+	public Utente(String username, String password, String nome, String cognome) {
+		this.username = username;
+		this.password = password;
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
--- src/main/webapp/navbar.jsp
+++ src/main/webapp/navbar.jsp
@@ -28,7 +28,7 @@
         </ul>
       </div>
       <div class="col-md-3 text-end">
-        <a  class="btn btn-primary" href="login.jsp">Login</a>
+        <a  class="btn btn-primary" href="LogoutServlet">Logout</a>
       </div>
     </div>
   </nav>
